ab35d8ab61936b30ce446e33440c762aae1c5ec7
Eugene Wang <eugene.wang.77@gmail.com>
2013-04-24 14:38:48 -0400

    refactoring

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -25,9 +25,10 @@ class Photo < ActiveRecord::Base
     end
   end
 
-  def self.save_tagged_photos
-    photos = InstagramWrapper.new.tag_recent_media({:tag => 'warbyparker'})
+  def self.save_tagged_photos(instagram_tag)
+    photos = InstagramWrapper.new.tag_recent_media({:tag => instagram_tag})
     photos.each do |photo|
+      # Creating a photo and associating the Instagram image properties
       p = Photo.where(:instagram_id => photo.id).first_or_create
       p.user_name = photo.user.username
       p.caption = photo.caption.text if photo.caption
@@ -38,25 +39,44 @@ class Photo < ActiveRecord::Base
       p.filter = photo.filter
       p.created_time = photo.created_time
       p.link = photo.link
-      photo.tags.each do |tag|
-        t = Tag.new
-        t.name = tag
-        t.save
-        p.tags << t
-      end      
-      photo.comments.data.each do |comment|
-        c = Comment.where(:comment_id => comment.id).first_or_create
-        c.comment = comment.text
-        c.comment_id = comment.id
-        c.save
-        p.comments << c
-      end
-      if photo.location
-        p.location = photo.location.name
-        p.latitude = photo.location.latitude
-        p.longitude = photo.location.longitude
-      end
+
+      # Saving tags
+      p.save_tags(photo)
+
+      # Saving comments  
+      p.save_comments(photo)
+
+      # Saving location
+      p.save_location(photo)
+
       p.save
+    end
+  end
+
+  def save_tags(instagram_photo)
+    instagram_photo.tags.each do |tag|
+      t = Tag.new
+      t.name = tag
+      t.save
+      self.tags << t
+    end 
+  end
+
+  def save_comments(instagram_photo)
+    instagram_photo.comments.data.each do |comment|
+      c = Comment.where(:comment_id => comment.id).first_or_create
+      c.comment = comment.text
+      c.comment_id = comment.id
+      c.save
+      self.comments << c
+    end
+  end
+
+  def save_location(instagram_photo)
+    if instagram_photo.location
+      self.location = instagram_photo.location.name
+      self.latitude = instagram_photo.location.latitude
+      self.longitude = instagram_photo.location.longitude
     end
   end
 
