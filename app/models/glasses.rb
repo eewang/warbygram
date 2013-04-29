2bb2bd42fb6269961d6cf4054c49111d9a2fc651
Erin L <email.erin@gmail.com>
2013-04-29 10:11:41 -0400

    update attribute accessible for glasses model

--- app/models/glasses.rb
+++ app/models/glasses.rb
@@ -1,5 +1,5 @@
 class Glasses < ActiveRecord::Base
-  attr_accessible :name, :type, :gender, :image, :style, :color, :collection, :optical, :sku, :male, :female, :active
+  attr_accessible :name, :image, :color, :collection, :optical, :sku, :male, :female, :active
   require 'nokogiri'
   require 'open-uri'
 
